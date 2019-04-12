80c1535c7182d0eb2f98acabff3f70bd12acf08d
dren39 <dren391@gmail.com>
2019-04-12 09:00:23 -0400

    added routes and navbar

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './Components/Home'
+import Welcome from './Components/Welcome';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Navbar = () =>
+<div>
+  <NavLink to='/welcome'>
+    Welcome
+  </NavLink>
+
+  <NavLink to='/app'>
+    Events
+  </NavLink>
+</div>
+
+ReactDOM.render((
+  <Router>
+    <React.Fragment>
+      <Navbar />
+      <Route path='/welcome' component={Welcome} />
+      <Route path='/app' component={App} />
+    </React.Fragment>
+  </Router>),
+// <App />
+
+document.getElementById('root')
+);
